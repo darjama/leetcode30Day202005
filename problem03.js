d099dca5b2289c36642f50b0edac014e57fd10e2
Daryl Marco <darmarco@gmail.com>
2020-05-03 05:33:33 -0700

    Added solution for 30-day challenge problem 3.

---
+++ problem03.js
@@ -0,0 +1,22 @@
+/**
+ * Ransom Note
+ * Given an arbitrary ransom note string and another string containing letters from all the magazines, write a function that will return true if the ransom note can be constructed from the magazines ; otherwise, it will return false.
+ * 
+ * Each letter in the magazine string can only be used once in your ransom note.
+ * 
+ * Note:
+ * You may assume that both strings contain only lowercase letters.
+ * 
+ * canConstruct("a", "b") -> false
+ * canConstruct("aa", "ab") -> false
+ * canConstruct("aa", "aab") -> true
+ * @param {string} ransomNote
+ * @param {string} magazine
+ * @return {boolean}
+ */
+var canConstruct = function(ransomNote, magazine) {
+  if (ransomNote.length === 0) return true;
+  let index = magazine.indexOf(ransomNote[0]);
+  if (index === -1) return false;
+  return canConstruct(ransomNote.substring(1), magazine.substring(0,index) + magazine.substring(index + 1))
+};
